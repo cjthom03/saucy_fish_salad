eff1eef9a9fd7b514e08c17a63309815554bc0dc
Charles Thomas <cjthom03@gmail.com>
2020-03-21 00:25:50 -0700

    get some randomness up ready for deploy

--- app/javascript/screens/home.js
+++ app/javascript/screens/home.js
@@ -3,7 +3,14 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return(
-      <div>THIS FISH!!</div>
+      <div>
+        <h1>ok BYEEEEE...</h1>
+        <br/>
+        <p>Welcome to my website. Thank you for visiting. Now, please go away.</p>
+        <p>I am not ready yet!</p>
+        <br/>
+        <p>Love, Charlie :)</p>
+      </div>
     )
   }
 }
