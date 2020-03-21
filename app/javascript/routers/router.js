a1331d478c57c89f7e0807fa54854f2b7acec646
Charles Thomas <cjthom03@gmail.com>
2020-03-21 00:01:04 -0700

    forgot the pack files

---
+++ app/javascript/routers/router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
+
+class SaucyFishRouter extends React.Component {
+  render() {
+    return 'FISH!'
+  }
+}
+
+export default SaucyFishRouter;
