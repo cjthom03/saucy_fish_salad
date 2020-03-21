b305610879e0284a86b335f92f8374bff2da3899
Charles Thomas <cjthom03@gmail.com>
2020-03-21 00:00:49 -0700

    get a basic router setup working

--- app/javascript/packs/root.js
+++ app/javascript/packs/root.js
@@ -1,3 +1,8 @@
 import React from  'react';
+import ReactDOM from 'react-dom';
+import SaucyFishRouter from '../routers/router';
 
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<SaucyFishRouter />, document.querySelector('main').appendChild(document.createElement('div')))
+});
 
