6a25631133e8b7d343a8f14c8627e389285a2d51
Charles Thomas <cjthom03@gmail.com>
2020-03-24 18:57:11 -0700

    add a route for random recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  scope :api, defaults: { format: :json } do
+    resources :recipes, only: [] do
+      collection { get :random }
+    end
+  end
+
   get '*page', to: 'root#index', constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
